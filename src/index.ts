08da6f7375239a9832778df97b010e60363190fc
bluesDD <hmori@interworks.jp>
2020-02-11 21:21:17 +0900

    :tada: Roleをリスト表示できた

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,34 @@
-function hello(name: string): string {
-    return `Hello, ${name}!`;
-  }
-  
-console.log(hello("Tep"));
+import { IAM } from "aws-sdk";
+
+class ListIamRole {
+	readonly iam: IAM
+
+	constructor(iam: IAM) {
+		this.iam = iam;
+	}
+
+	listAttachedRolePolicies () {
+		this.iam.listAttachedRolePolicies((err, data) => {
+			if (err) console.log(err, err.stack);
+			else console.log(data)
+		})
+	}
+
+	listRoleNames () {
+		this.iam.listRoles((err, data) => {
+			if (err) console.log(err, err.stack);
+			else { 
+				const roleNames = data.Roles.map(roleInfo => {
+					return roleInfo.RoleName
+				});
+				console.log(roleNames);
+			}
+		})
+	}
+}
+
+const iam = new IAM();
+
+const listiamrole = new ListIamRole(iam);
+
+listiamrole.listRoleNames();
